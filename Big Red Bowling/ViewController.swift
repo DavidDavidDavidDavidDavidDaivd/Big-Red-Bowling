43d4cd9154ea7ec4f8dba5de851846f432bdae63
David Bertuch <dcb277@cornell.edu>
2021-11-13 22:42:23 -0500

    Initial Commit

---
+++ Big Red Bowling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Big Red Bowling
+//
+//  Created by David Bertuch on 11/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
